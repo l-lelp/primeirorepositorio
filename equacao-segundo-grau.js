c3aa4ee47720cc4950b98c05fd9065dadac064c2
Joao França <francapedro10@gmail.com>
2022-10-10 17:48:09 -0300

    achei meio chato mas tudo bem provavelmente errei em alguma coisa

---
+++ equacao-segundo-grau.js
@@ -0,0 +1,16 @@
+// tentando fazer uma equacao do segundo grau usando minimamente a lógica
+// passo 1: simples equação
+
+let a = 5
+let b = 5
+let c = 3
+
+console.log(a + b - c)
+
+//passo 2: tentando usar um cadinho de álgebra
+
+const x = 4
+const y = 10
+const h = 3
+
+console.log(x + y*x - h**h)
