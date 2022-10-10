5b7175c7da960c0eb7f071424d4acf9856a982b4
Joao França <francapedro10@gmail.com>
2022-10-10 16:53:16 -0300

    apenas treinando algumas variáveis sem sentido

---
+++ treinando.js
@@ -0,0 +1,13 @@
+const Email = 'francapedro10@gmail.com'
+const Nome = 'Joao Pedro de Souza França' 
+const Idade = 20
+
+console.log(typeof(Email))
+console.log(typeof(Nome))
+console.log(typeof(Idade))
+
+let assunto = 'nós iremos almoçar 12:00'
+let respostaRemetente = 'soube que nesse horário o restaurante já está fechado'
+let escopo = 'quando o restaurante irá abrir?'
+
+console.log(assunto, respostaRemetente, escopo) * console.log(Email,Nome)
