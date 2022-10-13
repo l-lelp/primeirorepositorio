a89da486194e5c248a3b6c167c967957608c4ce9
Joao França <francapedro10@gmail.com>
2022-10-12 21:28:47 -0300

    primeira funcao

---
+++ ex-2.js
@@ -0,0 +1,28 @@
+/* Função sem parâmetro e sem retorno
+
+1) Função que horas são?
+Vamos criar uma função que quando chamada ela mostra no console a seguinte mensagem: Olá, agora são 10:20:43 PM
+*/
+
+function queHorasSao() {
+    const horaAtual = new Date();
+    const horaConvertida = horaAtual.toLocaleTimeString();
+    console.log("Olá, agora são " + horaConvertida);
+}
+
+queHorasSao()
+queHorasSao()
+queHorasSao()
+queHorasSao()
+queHorasSao()
+queHorasSao()
+queHorasSao()
+queHorasSao()
+queHorasSao()
+queHorasSao()
+queHorasSao()
+queHorasSao()
+queHorasSao()
+queHorasSao()
+queHorasSao()
+
