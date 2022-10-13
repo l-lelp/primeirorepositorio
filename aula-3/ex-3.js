64d899b3bd6f72791f2513452af7ef04d1382d3c
Joao França <francapedro10@gmail.com>
2022-10-13 11:19:14 -0300

    aplicando funcoes em numeros aleatorios

--- aula-3/ex-3.js
+++ aula-3/ex-3.js
@@ -1,11 +1,25 @@
 // Criando número inteiro aleatório em função
 
+function debug(variavel) {
+    
+    const tipo = typeof variavel
+
+    console.log(`[DEBUG] Valor: ${variavel} - Tipo: ${tipo}`)
+
+}
+
 function numeroAleatorio() {
     const min = 3
     const max = 10
     const numero = Math.random()*(max - min) + min
+    const numeroInteiro = Math.floor(numero)
 
-    return parseInt(numero);
+    debug(min)
+    debug(max)
+    debug(numero)
+    debug(numeroInteiro)
+
+    return numero
 }
 
 console.log(numeroAleatorio())
---
+++ aula-3/ex-4.js
@@ -0,0 +1,14 @@
+// Criando função debug
+
+function debug(variavel) {
+    
+    const tipo = typeof variavel
+
+    console.log(`[DEBUG] Valor: ${variavel} - Tipo: ${tipo}`)
+}
+
+debug("Joao")
+
+debug(20)
+
+debug("Itaboraí/RJ")
