cc71a215302b1705608d04c6750bcca783bcf533
Joao França <francapedro10@gmail.com>
2022-10-14 20:16:56 -0300

    tentando relembrar

--- terceira.js
+++ terceira.js
@@ -3,12 +3,12 @@
 2) Fazer sentido e ser breve
 */
 
-let nome = "Joao Franca"
-let email = "francapedro10@gmail.com" 
-let assunto = "Conversar sobre as declarações interraciais do Toguro"
+let nome = "Joao Franca";
+let email = "francapedro10@gmail.com";
+let assunto = "Conversar sobre as declarações interraciais do Toguro";
 let idade = 20 
-let mensagem = "Eai fellas! No video de hoje nos iremos você conhece o Elon Musk?? Ta criando sorvete" 
+let mensagem = "Eai fellas! No video de hoje nos iremos você conhece o Elon Musk?? Ta criando sorvete";
 
 const todo = (nome + email + assunto + idade + mensagem)
 
-console.log(todo)
+console.log(typeof(todo))
