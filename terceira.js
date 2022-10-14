c07b1ec55ea09e5eb4ca72753e4213b9098ece5f
Joao França <francapedro10@gmail.com>
2022-10-14 20:03:28 -0300

    ??

---
+++ terceira.js
@@ -0,0 +1,14 @@
+/* Escrevendo uma carta para ninguém
+1) tentar ativar todos os comandos de uma só vez
+2) Fazer sentido e ser breve
+*/
+
+let nome = "Joao Franca"
+let email = "francapedro10@gmail.com" 
+let assunto = "Conversar sobre as declarações interraciais do Toguro"
+let idade = 20 
+let mensagem = "Eai fellas! No video de hoje nos iremos você conhece o Elon Musk?? Ta criando sorvete" 
+
+const todo = (nome + email + assunto + idade + mensagem)
+
+console.log(todo)
