1f955ab03963697a04b2c464eddb11da5009a978
Joao França <francapedro10@gmail.com>
2022-10-16 18:56:17 -0300

    primeira situacao condicional

---
+++ aula-4/exp-1.js
@@ -0,0 +1,16 @@
+/* Site de Poker.
+
+nome: João
+ano: 2001
+
+*/
+
+const nome = "João"
+const ano = 2010
+
+if (2022 - nome >= 18) {
+    console.log("Pode jogar");
+}
+else {
+    console.log("Acesso proibido!")
+}
