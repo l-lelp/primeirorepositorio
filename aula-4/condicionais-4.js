b41b97e933a94c126fda2596b6be26b3169a749b
Joao França <francapedro10@gmail.com>
2022-10-16 19:36:49 -0300

    colinha de condicionais (como aplicar)

---
+++ aula-4/condicionais-4.js
@@ -0,0 +1,11 @@
+/* O número é Par ou Impar?
+*/
+
+const numero = 1
+const ehPar = numero % 2 == 0
+
+if (ehPar) {
+    console.log(`O número ${numero}é par.`)
+} else {
+    console.log(`o número ${número} é ímpar.`)
+}
