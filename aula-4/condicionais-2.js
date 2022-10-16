f579e1702f662bf02301cdb379e59c560f881aec
Joao França <francapedro10@gmail.com>
2022-10-16 19:10:52 -0300

    melhorou!

---
+++ aula-4/condicionais-2.js
@@ -0,0 +1,18 @@
+/* Site de Poker.
+
+nome: João
+ano: 2001
+
+*/
+
+const anoAtual = 2022;
+const nome = "João";
+const ano = 2010;
+const idade = anoAtual - ano;
+
+if (idade >= 18) {
+    console.log("Pode jogar");
+}
+else {
+    console.log("Acesso proibido!")
+}
