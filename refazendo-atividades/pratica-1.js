077aac256856195455880327e637a1aff1554505
Joao França <francapedro10@gmail.com>
2022-10-19 20:58:56 -0300

    revisei

---
+++ refazendo-atividades/pratica-1.js
@@ -0,0 +1,34 @@
+
+
+const nome = "Maria Augusta"
+const email = "mariaaugusta@gmail.com"
+const profissao = "Programadora"
+const idade = 32
+const lugarDosSonhos = "Nova Iorque"
+const nome = "Maria Augusta";
+const email = "mariaaugusta@gmail.com";
+const profissao = "Programadora";
+const idade = 32;
+const lugarDosSonhos = "Nova Iorque";
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugarDosSonhos)
+console.log(nome);
+console.log(email);
+console.log(profissao);
+console.log(idade);
+console.log(lugarDosSonhos);
+
+console.log(typeof nome)
+console.log(typeof email)
+console.log(typeof profissao)
+console.log(typeof idade)
+console.log(typeof lugarDosSonhos)
+console.log(typeof nome);
+console.log(typeof email);
+console.log(typeof profissao);
+console.log(typeof idade);
+console.log(typeof lugarDosSonhos);
