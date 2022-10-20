d5f536f440283cdd5a4af1c0176cd94fe9db4106
Joao França <francapedro10@gmail.com>
2022-10-20 16:38:37 -0300

    lista simplona

---
+++ aula-5/ex-7.js
@@ -0,0 +1,11 @@
+/*
+Ex-7: Crie uma função que retorna uma lista com números preenchidos de 1 a 10
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+*/
+
+function geradorDeLista() {
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+}
+
+console.log(geradorDeLista());
