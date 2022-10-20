fb8b92ff293b5a9f7510cbe068d3d4dc89c8ddb6
Joao França <francapedro10@gmail.com>
2022-10-20 16:41:05 -0300

    tao simples quanto

---
+++ aula-5/ex-8.js
@@ -0,0 +1,23 @@
+/*
+Ex-7: Crie uma função que retorna uma lista com números preenchidos de 1 a 10
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+*/
+
+function geradorDeLista() {
+let lista = []
+lista[0] = 1;
+lista[1] = 2;
+lista[2] = 3;
+lista[3] = 4;
+lista[4] = 5;
+lista[5] = 6;
+lista[6] = 7;
+lista[7] = 8;
+lista[8] = 9;
+lista[9] = 10;
+
+return lista;
+
+}
+
+console.log(geradorDeLista());
