1a7ea0f95394c2257dbdd03e2c26ac5f2857afa7
Joao França <francapedro10@gmail.com>
2022-10-20 15:39:12 -0300

    primeiro loop

---
+++ aula-5/ex-1.js
@@ -0,0 +1,12 @@
+/* Dominando Loops
+
+Ex1: Exibindo números de 1 a 10
+Crie uma função que quando chamada ela mostra no console números de 1 a 10
+*/
+
+let i = 1;
+
+while(i <= 10) {
+    console.log(i);
+    i = i + 1;
+}
