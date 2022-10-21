a7e89d751820794e4e320560a0277dc18bf5901d
Joao França <francapedro10@gmail.com>
2022-10-21 19:03:21 -0300

    treinando de novo pra relembrar

---
+++ treinim/ex-1.js
@@ -0,0 +1,9 @@
+//Exibindo tipo de variável
+
+const nome = "João Pedro"
+const email = 'francapedro10@gmail.com'
+const profissao = 'Autônomo'
+const idade = 20
+const lugarQueSonhaConhecer = 'Dubai'
+
+console.log(nome,email, profissao, idade, lugarQueSonhaConhecer)
