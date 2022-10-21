37fdbe5486bf6f0956d5f6c103b3109901f98f57
Joao França <francapedro10@gmail.com>
2022-10-21 19:31:05 -0300

    eh string ou n eh

---
+++ treinim/ex-4.js
@@ -0,0 +1,13 @@
+// Verificando se é uma String ou não
+
+const nome = "João Pedro"
+const email = 'francapedro10@gmail.com'
+const profissao = 'Autônomo'
+const idade = 20
+const lugarQueSonhaConhecer = 'Dubai'
+
+console.log(typeof nome === "string")
+console.log(typeof email === "string")
+console.log(typeof profissao === "string")
+console.log(typeof idade === "string")
+console.log(typeof lugarQueSonhaConhecer === "string")
