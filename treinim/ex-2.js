59f4d2d39ef751581c52556f34860e22125cb131
Joao França <francapedro10@gmail.com>
2022-10-21 19:05:46 -0300

    revisando strings

---
+++ treinim/ex-2.js
@@ -0,0 +1,13 @@
+//Exibindo tipo de variável
+
+const nome = "João Pedro"
+const email = 'francapedro10@gmail.com'
+const profissao = 'Autônomo'
+const idade = 20
+const lugarQueSonhaConhecer = 'Dubai'
+
+console.log(typeof(nome))
+console.log(typeof(email))
+console.log(typeof(profissao))
+console.log(typeof(idade))
+console.log(typeof(lugarQueSonhaConhecer))
