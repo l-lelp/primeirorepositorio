532e49ce672af586d2b4feb0b2877df6047ca382
Joao França <francapedro10@gmail.com>
2022-10-22 23:52:44 -0300

    pelo menos codei

--- treinim/ex-4.js
+++ treinim/ex-4.js
@@ -1,4 +1,4 @@
-// Verificando se é uma String ou não
+// Preparando uma mensagem de WhatsApp
 
 const nome = "João Pedro"
 const email = 'francapedro10@gmail.com'
@@ -6,8 +6,4 @@ const profissao = 'Autônomo'
 const idade = 20
 const lugarQueSonhaConhecer = 'Dubai'
 
-console.log(typeof nome === "string")
-console.log(typeof email === "string")
-console.log(typeof profissao === "string")
-console.log(typeof idade === "string")
-console.log(typeof lugarQueSonhaConhecer === "string")
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugarQueSonhaConhecer}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
---
+++ treinim/ex-5.js
@@ -0,0 +1,11 @@
+// Preparando uma mensagem de WhatsApp
+
+const nome = "João Pedro"
+const email = 'francapedro10@gmail.com'
+const profissao = 'Autônomo'
+const idade = 20
+const lugarQueSonhaConhecer = 'Dubai'
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugarQueSonhaConhecer}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
+
+console.log("hello world!")
