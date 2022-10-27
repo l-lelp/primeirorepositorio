8f65f7d07f21ce5558e0b4734e7dcfbcc9321121
Joao França <francapedro10@gmail.com>
2022-10-27 14:13:55 -0300

    entendendo a logica

---
+++ random/pratica-5.js
@@ -0,0 +1,28 @@
+// criando uma função debug gourmet
+
+function funcaoDebug (debugar) {
+    const tipo = typeof debugar
+    console.log(`[DEBUG] Valor: ${debugar} Tipo: ${tipo}`)
+}
+
+let max = 10
+let min = 5 
+const nAleatorios = (Math.random() * (max - min) + 5)
+let nnAleatorios = parseInt(nAleatorios)
+
+funcaoDebug (nnAleatorios)
+funcaoDebug (nnAleatorios)
+funcaoDebug (nnAleatorios)
+funcaoDebug (nnAleatorios)
+funcaoDebug (nnAleatorios)
+funcaoDebug (nnAleatorios)
+funcaoDebug (nnAleatorios)
+funcaoDebug (nnAleatorios)
+funcaoDebug (nnAleatorios)
+funcaoDebug (nnAleatorios)
+funcaoDebug (nnAleatorios)
+funcaoDebug (nnAleatorios)
+funcaoDebug (nnAleatorios)
+funcaoDebug (nnAleatorios)
+funcaoDebug (nnAleatorios)
+funcaoDebug (nnAleatorios)
