25269b15231e7ccf7f6341b8c96ba982d5aa2428
Joao França <francapedro10@gmail.com>
2022-10-27 14:19:23 -0300

    rebuscando o irrebuscável

---
+++ random/pratica-4.js
@@ -0,0 +1,7 @@
+// Calcular o valor de desconto de um determinado produto
+
+const carro = 12000
+const desconto = 23/100
+const resultado = (carro) * (1 - desconto)
+
+console.log(`"O valor do seu futuro carro com o desconto aplicado será R$ ${resultado}. Qual será a forma de pagamento? "`)
