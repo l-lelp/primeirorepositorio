0afca7ccbae6cc5e69a0eb5f04beafdb090d19b2
Joao França <francapedro10@gmail.com>
2022-10-27 11:26:31 -0300

    teste de funçao

---
+++ random/pratica-1.js
@@ -0,0 +1,8 @@
+/* Que horas são?
+A prática é dar uma pesquisada em algumas linhas de código e exercitar um pouco do JS
+1o Exercício: Responder com o horário atual.
+*/
+
+const hora = new Date
+
+console.log("Olá, excelentíssimo João, agora são exatamente " + hora)
