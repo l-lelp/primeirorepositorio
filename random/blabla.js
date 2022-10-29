cea90654204d8853102641f2151ecf0e2462d648
Joao França <francapedro10@gmail.com>
2022-10-29 14:30:06 -0300

    gerei a dor?

---
+++ random/blabla.js
@@ -0,0 +1,14 @@
+// Pares
+
+function geradorDeBla (n) {
+    let i = 1
+    let bla = ""
+
+    while (i <= n) {
+        bla = bla + "bla"
+        i++
+    }
+    return bla;
+}
+
+console.log(geradorDeBla(350))
---
+++ random/pares.js
@@ -0,0 +1,13 @@
+// Pares
+
+function pares (n) {
+    let i = 2
+    i % 2
+
+    while (i <= n) {
+        console.log(i)
+        
+    }
+}
+
+pares (350)
