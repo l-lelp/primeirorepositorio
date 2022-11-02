1b4fecd6f2a4e909d27ee9e7babac31de22d669c
Joao França <francapedro10@gmail.com>
2022-11-02 19:53:22 -0300

    n foi dessa vez

---
+++ treinim/treino-3.js
@@ -0,0 +1,12 @@
+/* Criar função para facilitar no preço do frete
+1º ser prático
+2º responder de forma simpática
+*/
+
+function calculaFrete (distancia, valor) {
+    let valorPiscina = ("")
+    const valorKm = 6.50 *(distancia)
+   console.log(valorKm)
+}
+
+calculaFrete(100)
