74260f563f7c056216a51032af31bd360a7eb495
Joao França <francapedro10@gmail.com>
2022-11-03 12:54:15 -0300

    mais facil q tirar doce da geladeira

--- treinim/treino-4.js
+++ treinim/treino-4.js
@@ -1,8 +1,13 @@
 // Exibindo números de 1 a 10
 
+function numerosDe1a10 () {
+
 let i = 1
 
 while (i <= 10) {
     console.log(i)
     i++
 }
+}
+
+numerosDe1a10()
---
+++ treinim/treino-5.js
@@ -0,0 +1,13 @@
+// Exibindo números de 1 a n
+
+function numerosDe1aN (n) {
+
+    let i = 1
+    
+    while (i <= n) {
+        console.log(i)
+        i++
+    }
+    }
+    
+    numerosDe1aN(10000)
