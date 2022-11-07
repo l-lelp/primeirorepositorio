7bb71b86949678fce40c4839b3a1433ad5e16994
Joao França <francapedro10@gmail.com>
2022-11-07 20:10:22 -0300

    voltando do 0

---
+++ treinim/tarefa-1.js
@@ -0,0 +1,8 @@
+// Exibindo tipo de Variável //
+
+const nome = "João P. de S. França"
+const email = "francapedro10@gmail.com"
+const profissao = "Autônomo"
+const idade = 21
+const lugarQueSonhoConhecer = "Balneário Camboriú"
+
