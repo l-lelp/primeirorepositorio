cea815c28606699a3a6f0d2ed2f48e6d9305aa03
Joao França <francapedro10@gmail.com>
2022-11-08 18:34:35 -0300

    nem tudo se resolve com funcão - eu:

--- treinim/tarefa-5.js
+++ treinim/tarefa-5.js
@@ -1,5 +1,9 @@
-const MILHAPARAKM = 1.60934;
-const distanciaTerraLuaEmMilhas = 238900;
-const distanciaTerraLuaEmKm = 238900*MILHAPARAKM;
+// Mensagem do zap com o Javascript
 
-console.log(distanciaTerraLuaEmKm);
+const nome = "João P. de S. França"
+const email = "francapedro10@gmail.com"
+const profissao = "Autônomo"
+const idade = 21
+const lugarQueSonhoConhecer = "Balneário Camboriú"
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugarQueSonhoConhecer}. Confirma pra gente alguns dados? A sua idade é ${idade} você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
---
+++ treinim/tarefa-6.js
@@ -0,0 +1,23 @@
+// Tabuada do 123
+
+
+function calculadora (x) {
+    const calculadoraDo123 = x*123
+    console.log(calculadoraDo123)
+}
+
+calculadora(1)
+calculadora(2)
+calculadora(3)
+calculadora(4)
+calculadora(5)
+calculadora(6)
+calculadora(7)
+calculadora(8)
+calculadora(9)
+calculadora(10)
+
+
+
+
+
